0615e0fcdfe9191504c0402a174389e54a434e9c
nayechan <nayechan1121@gmail.com>
2025-09-11 17:45:58 +0900

    Test Commit

--- Engine/UControlPanel.cpp
+++ Engine/UControlPanel.cpp
@@ -177,11 +177,6 @@ void UControlPanel::CameraManagementSection()
 	changed |= ImGui::DragFloat("##fov_drag", &fovDeg, dragSpeed, minFov, maxFov, "%.1f",
 		ImGuiSliderFlags_AlwaysClamp);
 
-	// 직접 입력 박스(원하면 유지, 아니면 생략 가능)
-	ImGui::SameLine();
-	ImGui::SetNextItemWidth(avail * 0.30f);
-	changed |= ImGui::InputFloat("##fov_input", &fovDeg, 0.0f, 0.0f, "%.3f");
-
 	// 리셋 버튼(옵션)
 	ImGui::SameLine();
 	if (ImGui::Button("Reset")) { fovDeg = 60.0f; changed = true; }
@@ -218,29 +213,30 @@ void UControlPanel::CameraManagementSection()
 		ImGui::EndTable();
 	}
 
-	ImGui::Text("Camera Rotation");
-	if (ImGui::BeginTable("CameraRotTable", 3, ImGuiTableFlags_BordersInnerV | ImGuiTableFlags_SizingStretchProp))
-	{
-		ImGui::TableNextRow(ImGuiTableRowFlags_Headers);
-		ImGui::TableSetColumnIndex(0); ImGui::Text("Pitch");
-		ImGui::TableSetColumnIndex(1); ImGui::Text("Yaw");
-		ImGui::TableSetColumnIndex(2); ImGui::Text("Roll");
+	//ImGui::Text("Camera Rotation");
+	//if (ImGui::BeginTable("CameraRotTable", 3, ImGuiTableFlags_BordersInnerV | ImGuiTableFlags_SizingStretchProp))
+	//{
+	//	ImGui::TableNextRow(ImGuiTableRowFlags_Headers);
+	//	ImGui::TableSetColumnIndex(0); ImGui::Text("Pitch");
+	//	ImGui::TableSetColumnIndex(1); ImGui::Text("Yaw");
+	//	ImGui::TableSetColumnIndex(2); ImGui::Text("Roll");
 
-		// Camera Rotation 행
-		ImGui::TableNextRow();
-		for (int32 i = 0; i < 3; i++)
-		{
-			ImGui::TableSetColumnIndex(i);
-			ImGui::SetNextItemWidth(-1);
-			ImGui::InputFloat(("##rot" + std::to_string(i)).c_str(),
-				&eulerXYZ[i], 0.0f, 0.0f, "%.3f");
-			if (ImGui::IsItemDeactivatedAfterEdit())
-				rotCommitted = true;
-		}
-		// ImGui::TableSetColumnIndex(3);
+	//	// Camera Rotation 행
+	//	ImGui::TableNextRow();
+	//	for (int32 i = 0; i < 3; i++)
+	//	{
+	//		ImGui::TableSetColumnIndex(i);
+	//		ImGui::SetNextItemWidth(-1);
+	//		ImGui::InputFloat(("##rot" + std::to_string(i)).c_str(),
+	//			&eulerXYZ[i], 0.0f, 0.0f, "%.3f");
+	//		if (ImGui::IsItemDeactivatedAfterEdit())
+	//			rotCommitted = true;
+	//	}
+	//	// ImGui::TableSetColumnIndex(3);
 
-		ImGui::EndTable();
-	}
+	//	ImGui::EndTable();
+	//}
+
 	// World / Local 선택 (체크박스 대신, 버튼처럼 보이는 상호배타 토글)
 
 	if (ModeButton("World", GizmoManager->GetIsWorldSpace()))
