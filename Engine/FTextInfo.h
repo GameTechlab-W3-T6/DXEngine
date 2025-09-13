d69d9f78a85678d5534f6cda17382bea21e11ec7
GameTithe <wkdwobum@gmail.com>
2025-09-13 14:29:54 +0900

    Align

--- Engine/FTextInfo.h
+++ Engine/FTextInfo.h
@@ -2,17 +2,20 @@
 #include "FTexture.h"
 #include "UEngineStatics.h"
  
+struct FMatrix;
 struct FTextInfo
 { 
 	std::vector<int> orderOfChar;
-
+	
 	int keyCode = 0;
+	float center;
 	FTexture* textTexture; 
-	   
 	int cellIndex[2];
+	   
 	float cellsPerRow, cellsPerColumn; 
 	float cellWidth, cellHeight;   
 	float size[2] = { 1.0f ,1.0f };
 
 	void SetParam(FTexture* textTex, int32 PerRow, int32 PerCol);
+	FMatrix MakeBillboard(FMatrix view);
 };
