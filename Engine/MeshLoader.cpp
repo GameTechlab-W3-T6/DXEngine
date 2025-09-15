c82d23a781b2f1749e747fa0cedb8a474cf09182
geb0598 <geb0598@gmail.com>
2025-09-15 23:32:59 +0900

    feat: MeshLoader 추가

---
+++ Engine/MeshLoader.cpp
@@ -0,0 +1,135 @@
+﻿#include <sstream>
+#include <random>
+
+#include "MeshLoader.h"
+
+#include "FVertexPosColor.h"
+
+template<>
+inline std::pair<TArray<FVertexPosColor>, TArray<UINT>> MeshLoader::LoadMesh(const std::filesystem::path& FilePath)
+{
+	LoadMeshImpl(FilePath);
+	
+	TArray<FVertexPosColor> VertexArray;
+	TArray<UINT> IndexArray;
+	TMap<std::tuple<int32, int32, int32>, UINT> IndexMap;
+	for (const auto& Face : Faces)
+	{
+		auto FaceKey = std::make_tuple(Face.PositionIndex, Face.NormalIndex, Face.TexCoordIndex);
+
+		if (IndexMap.find(FaceKey) != IndexMap.end())
+		{
+			IndexArray.push_back(IndexMap[FaceKey]);
+		}
+		else
+		{
+			IndexMap[FaceKey] = VertexArray.size();
+			FVertexPosColor Vertex;
+			if (Face.PositionIndex != -1)
+			{
+				FVector Position = Positions[Face.PositionIndex];
+				Vertex.x = Position.X;
+				Vertex.y = Position.Y;
+				Vertex.z = Position.Z;
+			}
+			if (Face.NormalIndex != -1)
+			{
+				FVector Normal = Normals[Face.NormalIndex];
+				// DO NOTHING
+			}
+			if (Face.TexCoordIndex != -1)
+			{
+				FVector2 TexCoord = TexCoords[Face.TexCoordIndex];
+				// DO NOTHING
+			}
+			/* COLOR */
+			std::random_device RandomDevice;
+			std::mt19937 Gen(RandomDevice());
+			std::uniform_real_distribution<float> Dist(0.0f, 1.0f);
+			Vertex.r = Dist(Gen);
+			Vertex.g = Dist(Gen);
+			Vertex.b = Dist(Gen);
+			Vertex.a = 1.0f;
+		}
+
+		return { VertexArray, IndexArray };
+	}
+}
+
+void MeshLoader::LoadMeshImpl(const std::filesystem::path& FilePath)
+{
+	if (!std::filesystem::exists(FilePath))
+	{
+		assert(false); /** @todo */
+	}
+
+	std::ifstream File(FilePath);
+	if (!File)
+	{
+		assert(false); /** @todo */
+	}
+
+	Positions.clear();
+	Normals.clear();
+	TexCoords.clear();
+	Faces.clear();
+
+	FString Line;
+	while (std::getline(File, Line))
+	{
+		std::istringstream Tokenizer(Line);
+
+		FString Prefix;
+		Tokenizer >> Prefix;
+
+		if (Prefix == "v")
+		{
+			FVector Position;
+			Tokenizer >> Position.X >> Position.Y >> Position.Z;
+			Positions.push_back(Position);
+		}
+		else if (Prefix == "vn")
+		{
+			FVector Normal;
+			Tokenizer >> Normal.X >> Normal.Y >> Normal.Z;
+			Normals.push_back(Normal);
+		}
+		else if (Prefix == "vt")
+		{
+			FVector2 TexCoord;
+			Tokenizer >> TexCoord.X >> TexCoord.Y;
+			TexCoords.push_back(TexCoord);
+		}
+		else if (Prefix == "f")
+		{
+			FString FaceBuffer;
+			while (Tokenizer >> FaceBuffer)
+			{
+				Faces.push_back(ParseFaceBuffer(FaceBuffer));
+			}
+		}
+	}
+}
+
+MeshLoader::FFace MeshLoader::ParseFaceBuffer(const FString& FaceBuffer)
+{
+	FFace Face = {};
+	std::istringstream Tokenizer(FaceBuffer);
+	FString IndexBuffer;
+
+	std::getline(Tokenizer, IndexBuffer, '/');
+
+	Face.PositionIndex = std::stoi(IndexBuffer) - 1;
+
+	if (std::getline(Tokenizer, IndexBuffer, '/') && !IndexBuffer.empty())
+	{
+		Face.TexCoordIndex = std::stoi(IndexBuffer) - 1;
+	}
+
+	if (std::getline(Tokenizer, IndexBuffer, '/') && !IndexBuffer.empty())
+	{
+		Face.NormalIndex = std::stoi(IndexBuffer) - 1;
+	}
+
+	return Face;
+}
---
+++ Engine/MeshLoader.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+#include <filesystem>
+
+#include "Vector.h"
+#include "TArray.h"
+#include "UEngineStatics.h"
+
+class MeshLoader
+{
+private:
+	struct FFace
+	{
+		int32 PositionIndex = -1;
+		int32 NormalIndex = -1;
+		int32 TexCoordIndex = -1;
+	};
+
+public:
+	static MeshLoader& GetInstance()
+	{
+		static MeshLoader Instance;
+		return Instance;
+	}
+
+	~MeshLoader() = default;
+
+	MeshLoader(const MeshLoader&) = delete;
+	MeshLoader(MeshLoader&&) = delete;
+
+	MeshLoader& operator=(const MeshLoader&) = delete;
+	MeshLoader& operator=(MeshLoader&&) = delete;
+
+	template<typename TVertex>
+	std::pair<TArray<TVertex>, TArray<UINT>>
+		LoadMesh(const std::filesystem::path& FilePath);
+
+private:
+	MeshLoader() = default;
+
+	void LoadMeshImpl(const std::filesystem::path& FilePath);
+
+	FFace ParseFaceBuffer(const FString& FaceBuffer);
+
+	TArray<FVector> Positions;
+	TArray<FVector> Normals;
+	TArray<FVector2> TexCoords;
+	TArray<FFace> Faces;
+};
+
